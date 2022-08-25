0237fe51505b58c7e4718c1ff417c3599901bd4d
Manu <manuellafflopes@gmail.com>
2022-08-25 21:54:38 +0100

    Praticar, praticar, praticar

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let velocidade = 50.5
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+if (velocidade>60) {
+    console.log(`Você ultrpassou a velocidade permitida, MULTADO`)
+}
+console.log(`Dirija sempre usando o cinto de segurança !`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+let pais = 'Brasil'
+console.log(`Vivendo em ${pais}, `)
+if (pais == 'Brasil') {
+    console.log('você é Brasileiro !:D')
+} else {
+    console.log(`você é estrangeiro.`)
+}
