8927f609ae7f9733ddd38ada3f5e9a8155eaa658
Manu <manuellafflopes@gmail.com>
2022-09-08 19:03:47 +0100

    While... Do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+//console.log('Tudo bem ?')
+
+// let contador = 1
+// while (contador <= 6) {
+//     console.log(`Passo ${contador}`)
+//     contador++
+// }
+
+let contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+ 
