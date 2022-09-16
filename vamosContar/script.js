3d5b2e43479ebbf8813e9b32eebf83d53f211d77
Manu <manuellafflopes@gmail.com>
2022-09-16 18:29:20 +0100

    Vamos contar ??

--- vamosContar/script.js
+++ vamosContar/script.js
@@ -1,10 +1,26 @@
 function contar() {
     let start = Number(document.querySelector('#txtStart').value)
     let end = Number(document.querySelector('#txtEnd').value)
-    let step = Number(document.querySelector('#txtStep').value)
+    let step = Math.abs(Number(document.querySelector('#txtStep').value))
     let resp = document.querySelector('#resp')
-    resp.innerHTML = `Contando...  `
-    for (let counter = start; counter <= end; counter += step) {
-        resp.innerHTML += `${counter} `
+    if (!start || !end || !step) {
+        window.alert(`[ERRO] Faltam dados !`)
+    } else {
+        resp.innerHTML = `Counting...  <br>`
+        if (start < end) {
+            //contagem crescente
+            for (let counter = start; counter <= end; counter += step) {
+                resp.innerHTML += ` ${counter} \u{1F449} `
+            }
+        } else {
+            //contagem descrescente
+            for (let counter = start; counter >= end; counter -= step) {
+                resp.innerHTML += ` ${counter} \u{1F449} `
+            }
+        }
+        resp.innerHTML += ` \u{1F596}.`
     }
+}
+function refresh() {
+    window.location.reload()
 }
